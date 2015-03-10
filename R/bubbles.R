d477885a717240aaa8ca9839c5274f4684aed591
jpmarindiaz <jpmarindiaz@gmail.com>
2015-03-10 12:37:39 -0500

    initial commit

---
+++ R/bubbles.R
@@ -0,0 +1,87 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+bubbles <- function(data, name = "bubbleForce", width = NULL, height = NULL) {
+
+#   if(is.null(data$id)) data$id <- seq(1:nrow(data))
+
+  if(!"data.frame" %in% class(data)) {
+    stop("data must be a dataframe")
+  }
+
+  if (is.null(data$x)){
+    message("No node x position provided: using random value")
+    data$x <- runif(nrow(data),1,10)
+  }
+
+  if (is.null(data$y)){
+    message("No node y position provided: using random value")
+    data$y <- runif(nrow(data),1,10)
+  }
+
+  if (is.null(data$radius)){
+    message("No radius provided: using fixed value = 5")
+    data$radius <- 5
+  }
+
+  if (is.null(data$cluster)){
+    data$cluster <- 0
+  }
+
+  clusters <- ddply(data,.(cluster),function(r){
+    r <- r[with(r,order(radius)),]
+    head(r,1)
+  })
+
+  n <- nrow(data)
+  m <- nrow(clusters)
+
+  # Convert to array of objects
+  data <- apply(data, 1,function(r){
+    as.list(r)
+  })
+  names(data) <- NULL
+
+  clusters <- apply(clusters, 1,function(r){
+    as.list(r)
+  })
+
+  x = list(
+    n = n,
+    m = m,
+    data = data,
+    clusters = clusters
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = name,
+    x,
+    width = width,
+    height = height,
+    package = 'bubbleCloud',
+    sizingPolicy = htmlwidgets::sizingPolicy(
+      viewer.padding = 0,
+      browser.fill = TRUE
+    )
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @export
+bubblesOutput <- function(outputId, width = '100%', height = '400px'){
+  shinyWidgetOutput(outputId, 'bubble', width, height, package = 'bubbleCloud')
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @export
+renderBubbles <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, dimplerOutput, env, quoted = TRUE)
+}
---
+++ inst/00.R
@@ -0,0 +1,25 @@
+
+library(devtools)
+library(htmlwidgets)
+
+
+devtools::install()
+library(bubbleCloud)
+
+
+data <- read.csv("inst/data/bubbles.csv")
+data2 <- data[1:50,]
+#bubbles(data)
+bubbles(data2[c("cluster")])
+
+bubbles(data2[,2:4])
+bubbles(data2[,2:4])
+
+data3 <- data
+data3$cluster <- 0
+bubbles(data3)
+
+
+#data <- data[,1]
+#bubbles(data)
+
