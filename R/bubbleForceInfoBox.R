a6db5661871cb702daa652909fceefa8f355e58d
jpmarindiaz <jpmarindiaz@gmail.com>
2015-03-16 19:45:38 -0500

    infobox

--- R/bubbleForceInfoBox.R
+++ R/bubbleForceInfoBox.R
@@ -5,58 +5,65 @@
 #' @import htmlwidgets
 #'
 #' @export
-bubblesForceInfobox <- function(data, width = NULL, height = NULL) {
-  
+bubbleForceInfobox <- function(data, opts=list(initInfobox=NULL),width = NULL, height = NULL) {
+
   name <- "bubbleForceInfobox"
   #   if(is.null(data$id)) data$id <- seq(1:nrow(data))
-  
+
   if(!"data.frame" %in% class(data)) {
     stop("data must be a dataframe")
   }
-  
+
   if(is.null(is.null(data$htmlinfo))){
     message("No node htmlinfo: using default")
     data$htmlinfo <- "<h1>This</h1> is some <strong>html</strong>"
   }
-  
+
   if (is.null(data$x)){
     message("No node x position provided: using random value")
     data$x <- runif(nrow(data),1,400)
   }
-  
+
   if (is.null(data$y)){
     message("No node y position provided: using random value")
     data$y <- runif(nrow(data),1,400)
   }
-  
+
   if (is.null(data$radius)){
     message("No radius provided: using fixed value = 5")
     data$radius <- 5
   }
-  
+
   if (is.null(data$group)){
     message("No group variable")
     data$cluster <- 0
   } else{
     data$cluster <- as.numeric(factor(data$group))-1
   }
-  
-  
+
+  if(is.null(opts$initInfobox)){
+    initInfobox <- "Haz click en los nodos para ver detalles!"
+  }
+
+
   clusters <- ddply(data,.(cluster),function(r){
     r <- r[with(r,order(radius)),]
     head(r,1)
   })
-  
+
   n <- nrow(data)
   m <- nrow(clusters)
-  
+
   #   # Convert to array of objects
   #   data <- apply(data, 1,function(r){
   #     as.list(r)
   #   })
   #   names(data) <- NULL
-  
-  
+
+  settings <- list(
+    initInfobox = initInfobox
+      )
+
   #   apply(data[,2:4], 1,function(r){
   #     message(r)
   #     #r <- head(data,1)
@@ -69,15 +76,18 @@ bubblesForceInfobox <- function(data, width = NULL, height = NULL) {
   #   clusters <- apply(clusters, 1,function(r){
   #     as.list(r)
   #   })
-  
+
+
+
   x = list(
+    settings = settings,
     n = n,
     m = m,
     data = data,
     clusters = clusters
   )
-  
-  
+
+
   # create widget
   htmlwidgets::createWidget(
     name = name,
@@ -95,7 +105,7 @@ bubblesForceInfobox <- function(data, width = NULL, height = NULL) {
 #' Widget output function for use in Shiny
 #'
 #' @export
-bubblesInfoboxOutput <- function(outputId, width = '100%', height = '400px'){
+bubbleInfoboxOutput <- function(outputId, width = '100%', height = '400px'){
   shinyWidgetOutput(outputId, 'bubbleForceInfobox', width, height, package = 'bubbleCloud')
 }
 
@@ -104,5 +114,5 @@ bubblesInfoboxOutput <- function(outputId, width = '100%', height = '400px'){
 #' @export
 renderInfoboxBubbles <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
-  shinyRenderWidget(expr, bubblesOutput, env, quoted = TRUE)
+  shinyRenderWidget(expr, bubbleInfoboxOutput, env, quoted = TRUE)
 }
